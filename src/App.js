e35ca1d4410ccf6d65b572a9277e1c8341c1966e
heheyoung <yangyang@yunjianvc.com>
2019-05-14 11:37:58 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState(() => {
+        return { a: 1, b: 2 };
+    });
+
+    return (
+        <div className='App'>
+            <p>a:{state.a}</p>
+            <p>b:{state.b}</p>
+            <button onClick={() => setState({ ...state, a: state.a + 1 })}>
+                +
+            </button>
+            <button
+                onClick={() =>
+                    setState(({ a, b }) => {
+                        return { a, b: b - 1 };
+                    })
+                }>
+                -
+            </button>
+            <Example />
+        </div>
+    );
+}
+
+function Example(props) {
+    let [name, setName] = useState("");
+    let [xxx, setX] = useState("");
+    let life = useRef();
+    useEffect(() => {
+        if (!life.current) {
+            life.current = true;
+        } else {
+            // didUpdate
+            console.log(1111);
+        }
+        setTimeout(() => {
+            setX(xxx + name);
+        }, 1000);
+        return () => {};
+    }, [name]);
+    return (
+        <>
+            <div>{xxx}</div>
+            <input
+                placeholder='请输入姓名'
+                type='text'
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+        </>
+    );
 }
 
 export default App;
